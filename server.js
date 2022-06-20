ec27047f36fd57709f4d06d0ebd3e2e0f8577316
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-06-20 10:29:10 -0400

    issue with port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const {uuid} = require('uuidv4');
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,7 +13,36 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '/'))
-}
+    res.sendFile(path.join(__dirname, '/public.index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('api/notes', (req, res) => {
+    const data = fs.readFileSync(path.join(__dirname, '/db/db.json'), "utf8", () => {})
+
+    return res.json(JSON.parse(data));
+});
+
+app.post('api/notes', (req, res) => {
+    let data = fs.readFileSync(path.join(__dirname, '/db/db.json'), "utf8", () => {});
+
+    data = JSON.parse(data);
+    req.body.id = uuid();
+    data.push(req.body);
+
+    data = JSON.stringify(data);
+
+    fs.writeFile(path.join(__dirname, '/db/db.json'), data, () => {});
+
+    return res.send("Note added")
+});
+
+
+app.listen(PORT, () => {
+    console.log("Notes at http://localhost:${PORT}")
+});
 
 
