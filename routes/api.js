1db5147a26141a195166078a8770adb40882ba33
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-08-05 20:28:23 -0400

    wont load wihtout typing in .html

--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,26 @@
-var db = require("/db/db.json");
+var db = require("../db/db.json");
 var fs = require("fs");
 
 module.exports = function(app) {
     app.get("/api/notes", function (req, res) {
-        res.join(db);
+        res.json(db);
     });
 
     app.post('/api/notes', function (req, res) {
         db.push(req.body);
-        db.foreach((obj, i) => {
+        db.forEach((obj, i) => {
             obj.id = i + 1;  
         });
 
         fs.writeFile("./db/db.json", JSON.stringify(db), function () {
-            res.join(db);
+            res.json(db);
         });
     });
 
     app.delete("/api/notes/:id", function (req, res) {
         var id = req.params.id;
         db.splice(id - 1, 1);
-        db.foreach((obj, i) => {
+        db.forEach((obj, i) => {
             obj.id = i + 1;
         });
 
